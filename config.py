27900d98efd231423d93f71c7cfb7a576b3d8314
HappyHippo77 <badpiggies927@gmail.com>
2020-09-20 04:29:58 -0400

    Actually added config.py this time.

---
+++ config.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------#
+#             Bot Configuration File              #
+#-------------------------------------------------#
+
+with open('token.txt','r') as file:
+    token = file.read()
+
+# System Basics
+token = token.strip()
+prefix = "?"
+description = "Oe skxawng asrunga' lu"
+version = "0.0.1"
+# Operators are people with override access to bot admin commands like reload
+operators = [423581502970789889,189504650645471232]
+status = "In Dev"
+
+help = """A list of all of Ewo's commands.
+Prefix: ?
+`kaltxì` Say hi
+
+All dictionary data from dictnavi.com.
+All example sentences were collected by Plumps for the Annotated Dictionary and provided for use here.
+"""
