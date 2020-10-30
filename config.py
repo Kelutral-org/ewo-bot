1566a7c4fd8e39f0b0e1e38841a7c8dece0372a3
skipper587 <45574121+skipper587@users.noreply.github.com>
2020-10-29 22:17:02 -0400

    Added Mako's id to operators

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ prefix = "?"
 description = "Oe skxawng asrunga' lu"
 version = "1.1.1"
 # Operators are people with override access to bot admin commands like reload
-operators = [423581502970789889,189504650645471232,205370567614922753,429361033446948864]
+operators = [423581502970789889,189504650645471232,205370567614922753,429361033446948864,81105065955303424]
 status = "In Dev"
 repo = r"C:\Users\Conner\Desktop\Coding\Python\Discord.py\Ewo'\.git"
 directory = r"C:\Users\Conner\Desktop\Coding\Python\Discord.py\Ewo'"
