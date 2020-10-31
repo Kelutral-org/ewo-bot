4e319cc201c9eec88576ffc9b6b72f19dec235f9
HappyHippo77 <badpiggies927@gmail.com>
2020-10-31 03:09:35 -0400

    Update 1.2.1

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ with open('token.txt','r') as file:
 token = token.strip()
 prefix = "?"
 description = "Oe skxawng asrunga' lu."
-version = "1.1.8"
+version = "1.2.1"
 operators = [423581502970789889,189504650645471232,205370567614922753,429361033446948864, 81105065955303424]
 bot_channel = 718309398048538687
 
