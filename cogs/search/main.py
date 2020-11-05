d4b289809452a95887f22c8a07d00eb621e1efac
The Unnamed Engineer <theunnamedengineer@gmail.com>
2020-11-05 18:39:42 -0500

    Clean search cog formatting

--- cogs/search/main.py
+++ cogs/search/main.py
@@ -7,8 +7,9 @@ import json
 with open("cogs/search/compact_database.json", encoding='utf-8') as f:
     search_database = json.load(f)
 
+
 class Search(commands.Cog):
-    def __init__(self,bot):
+    def __init__(self, bot):
         self.bot = bot
 
     @commands.command(name='search', aliases=['fwew', 'word', 'lì\'u', 'pelì\'u', 'suche'])
@@ -27,9 +28,10 @@ class Search(commands.Cog):
                 foundname = dictionary["name"]
                 info = ''
                 founddef = dictionary["definition"]
-                info = info + founddef+'\n'
+                info = info + founddef + '\n'
                 if 'p' in options:
-                    info = info + '\n' + bot.lang.get(str(ctx.guild.id)).get('part_of_speech') + ': ' + dictionary['pos']
+                    info = info + '\n' + bot.lang.get(str(ctx.guild.id)).get('part_of_speech') + ': ' + dictionary[
+                        'pos']
                 if 'i' in options:
                     info = info + '\nIPA: (' + dictionary['ipa'] + ')'
                 if 'f' in options:
@@ -37,7 +39,7 @@ class Search(commands.Cog):
                     stress = 0
                     for item in stresslist:
                         stress += 1
-                        if re.match('\u02c8',item):
+                        if re.match('\u02c8', item):
                             break
                     info = info + '\n' + bot.lang.get(str(ctx.guild.id)).get('stressed_syllable') + ': ' + str(stress)
                 if 's' in options:
@@ -65,15 +67,21 @@ class Search(commands.Cog):
                             resultinfo.append(info)
                     except:
                         continue
-        if resultnames == []:
-            await ctx.send(embed=discord.Embed(title=bot.lang.get(str(ctx.guild.id)).get('search_title'), description=bot.lang.get(str(ctx.guild.id)).get('search_no_results'), color=0xff0000))
-        elif len(resultnames)> 20:
-            await ctx.send(embed=discord.Embed(title=bot.lang.get(str(ctx.guild.id)).get('search_title'), description=bot.lang.get(str(ctx.guild.id)).get('search_many_results'), color=0xff0000))
+        if not resultnames:
+            await ctx.send(embed=discord.Embed(title=bot.lang.get(str(ctx.guild.id)).get('search_title'),
+                                               description=bot.lang.get(str(ctx.guild.id)).get('search_no_results'),
+                                               color=0xff0000))
+        elif len(resultnames) > 20:
+            await ctx.send(embed=discord.Embed(title=bot.lang.get(str(ctx.guild.id)).get('search_title'),
+                                               description=bot.lang.get(str(ctx.guild.id)).get('search_many_results'),
+                                               color=0xff0000))
         else:
             embed = discord.Embed(title=bot.lang.get(str(ctx.guild.id)).get('search_results_title'), color=899718)
             for i, item in enumerate(resultnames):
                 embed.add_field(name=item, value=resultinfo[i])
             await ctx.send(embed=embed)
+
+
 def setup(bot):
     bot.add_cog(Search(bot))
     print('Added new Cog: ' + str(Search))
