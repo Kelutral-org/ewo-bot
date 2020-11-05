341051b733ca65b1f906866633615a4d60a5ad7c
The Unnamed Engineer <theunnamedengineer@gmail.com>
2020-11-05 13:29:50 -0500

    Apply PEP8 practices to config file

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-#-------------------------------------------------#
-#             Bot Configuration File              #
-#-------------------------------------------------#
+# ------------------------------------------------- #
+#              Bot Configuration File               #
+# ------------------------------------------------- #
 
 # Open token file (so the repository doesn't receive the token and the config can still be pushed)
-with open('token.txt','r') as file:
+with open('token.txt', 'r') as file:
     token = file.read()
 
 # System Basics
@@ -11,7 +11,7 @@ token = token.strip()
 prefix = "?"
 description = "Oe skxawng asrunga' lu."
 version = "1.3.3"
-operators = [423581502970789889,189504650645471232,205370567614922753,429361033446948864, 81105065955303424]
+operators = [423581502970789889, 189504650645471232, 205370567614922753, 429361033446948864, 81105065955303424]
 
 # These need to be Anaru's directory
 repo = r"F:\Ewo\.git"
